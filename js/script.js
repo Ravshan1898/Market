e3d64eb96e0d819c7eb811b94aca5240d17bb004
Ravshan <you@example.com>
2024-07-28 16:36:56 +0500

    MARKET

---
+++ js/script.js
@@ -0,0 +1,27 @@
+var swiper = new Swiper(".mySwiper", {
+  effect: "cube",
+  grabCursor: true,
+  cubeEffect: {
+    shadow: true,
+    slideShadows: true,
+    shadowOffset: 20,
+    shadowScale: 0.94,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 4200,
+    disableOnInteraction: false,
+  },
+  slidesPerView: 1,
+  spaceBetween: 30,
+  keyboard: {
+    enabled: true,
+  },
+});
